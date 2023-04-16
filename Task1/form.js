c01328b1e5e7deeacef63891acf25c1ea1e7acbb
dhinesh454 <dhinesh454@gmail.com>
2023-04-16 11:26:49 +0530

    Task 4 forms kickstatrt

---
+++ Task1/form.js
@@ -0,0 +1,14 @@
+function registeruser()
+    {
+        const name=document.getElementById("name").value;
+        const email=document.getElementById("email").value;
+        const phone=document.getElementById("phone").value;
+        const date=document.getElementById("date").value;
+        const time=document.getElementById("time").value;
+        console.log(name)
+        console.log(email)
+        console.log(phone)
+        console.log(date)
+        console.log(time)
+      
+    }
